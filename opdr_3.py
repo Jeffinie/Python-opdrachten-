64a828dc33a59e9dd81db311f76607ebaa53bedd
Jeffinie <samnellestein@gmail.com>
2025-03-05 15:10:31 +0100

    opdracht 3 af

---
+++ opdr_3.py
@@ -0,0 +1,15 @@
+# Opdracht 3 
+# Naam student:Sam Nellestein 
+# Groep:It 2B 
+
+# Hier komt je code...
+
+my_dict = {
+"naam": "sam", 
+"achternaam": "nellestein",
+"leeftijd": "21", 
+"favorieten kleur": "rood", 
+
+}
+
+print(my_dict)
